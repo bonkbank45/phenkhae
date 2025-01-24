c4994875cb1f89ef7004a51674e05256214f8276
bonkBANK <bankforever22@gmail.com>
2025-01-24 20:27:51 +0700

    Fix learn_massage_des and work_massage_des not add to DB

--- back-end/app/Http/Requests/UpdateStudentRequest.php
+++ back-end/app/Http/Requests/UpdateStudentRequest.php
@@ -27,7 +27,7 @@ class UpdateStudentRequest extends FormRequest
             "lastname_tha" => "required|string|max:255",
             "firstname_eng" => "required|string|max:255",
             "lastname_eng" => "required|string|max:255",
-            "citizen_card" => "required|string|max:255|unique:students,citizenid_card",
+            "citizen_card" => "required|string|max:255",
             "birthdate" => "required|date",
             "birth_province_id" => "required|exists:provinces,id",
             "father_lname" => "required|string|max:255",
@@ -38,25 +38,16 @@ class UpdateStudentRequest extends FormRequest
             "address_moo" => "required|string|max:255",
             "address_road" => "required|string|max:255",
             "address_subdistrict_id" => "required|exists:subdistricts,id",
-            "address_zip_code" => "required|string|max:255",
             "phonenumber" => "required|string|max:255",
-            "email" => "required|string|max:255|email|unique:students,email",
+            "email" => "required|string|max:255|email",
             "occupation_id" => "required|exists:occupations,id",
             "medical_condition_id" => "required|exists:medical_conditions,id",
             "surgery_history" => "required|string|max:60",
             "learn_massage" => "required|integer|min:0|max:1",
-            "learn_massage_des" => "nullable|text|max:70",
+            "learn_massage_description" => "nullable|text|max:70",
             "work_massage" => "required|integer|min:0|max:1",
-            "work_massage_des" => "nullable|text|max:70",
+            "work_massage_description" => "nullable|text|max:70",
             "profile_image" => "nullable|image|mimes:jpeg,png,jpg|max:2048",
-        ];
-    }
-
-    public function messages()
-    {
-        return [
-            'citizenid_card.unique' => 'รหัสประจำตัวประชาชนนี้มีอยู่ในระบบแล้ว',
-            'email.unique' => 'อีเมลนี้มีอยู่ในระบบแล้ว',
         ];
     }
 }
