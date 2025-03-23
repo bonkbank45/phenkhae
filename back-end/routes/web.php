abcb83e620efe23fc9ac18da7c8879fc05d3ab9e
bonkBANK <bankforever22@gmail.com>
2025-03-23 22:05:08 +0700

    Add route web.php for run build vite by Laravel

--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
+// Route::get('/', function () {
+//     return ['Laravel' => app()->version()];
+// });
+Route::get('/{any}', function () {
+    $path = public_path('index.html');
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+    if (!File::exists($path)) {
+        abort(404);
+    }
 
-require __DIR__.'/auth.php';
+    return Response::file($path);
+})->where('any', '.*');
+
+require __DIR__ . '/auth.php';
